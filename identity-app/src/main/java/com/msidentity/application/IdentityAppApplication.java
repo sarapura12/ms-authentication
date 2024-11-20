8b31b25ed390f33c37825e5ea158741dbe4e0417
PabloSarapura97 <rapid12300@gmail.com>
2024-11-20 01:58:58 -0300

    setup project

---
+++ identity-app/src/main/java/com/msidentity/application/IdentityAppApplication.java
@@ -0,0 +1,13 @@
+package com.msidentity.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityAppApplication.class, args);
+    }
+
+}
---
+++ identity-app/src/test/java/com/msidentity/application/IdentityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msidentity.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
